667bcee53478b1f802c6f5fd1790d72d3d9c7a7a
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2022-10-24 22:37:06 +0100

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+// src/pages/_app.tsx
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
+import { ReactQueryDevtools, } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const queryClient = new QueryClient();
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </SessionProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
