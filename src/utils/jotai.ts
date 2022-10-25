efdd6fb1abc02313a59c51287abe27373807e008
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2022-10-25 16:17:55 +0100

    helper

---
+++ src/utils/jotai.ts
@@ -0,0 +1,17 @@
+import { atom } from "jotai";
+import { string } from "zod";
+
+export const generationState = atom<"generate" | "pause" | "disabled">(
+  "disabled"
+);
+
+export const gridState = atom("");
+export const codeState = atom({
+  value: "",
+  firstChar: "",
+  secondChar: "",
+  seconds: 0,
+  firstVal: 0,
+  lastVal: 0,
+  grid: "",
+});
