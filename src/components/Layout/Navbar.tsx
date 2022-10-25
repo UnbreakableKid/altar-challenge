615de0679e11537dd13b592f9097517cf3c5d519
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2022-10-25 16:17:23 +0100

    refactoring

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, chakra, CloseButton, Flex, HStack, IconButton, useColorMode, useColorModeValue, useDisclosure, VisuallyHidden, VStack } from '@chakra-ui/react';
+import { Button, chakra, Flex, HStack, useColorMode, useColorModeValue, useDisclosure } from '@chakra-ui/react';
 import React from 'react'
-import { BsMenuApp } from 'react-icons/bs';
 import NextLink from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 interface NavbarProps {
 
@@ -11,6 +11,8 @@ const Navbar = ({ }: NavbarProps) => {
     const bg = useColorModeValue("white", "gray.800");
     const mobileNav = useDisclosure();
     const { colorMode, toggleColorMode } = useColorMode();
+    const { data: sessionData } = useSession();
+
     return (<>
         <chakra.header
             bg={bg}
@@ -42,7 +44,15 @@ const Navbar = ({ }: NavbarProps) => {
                         </NextLink>
                     </HStack>
                 </HStack>
-                <Button onClick={toggleColorMode}> Toggle {colorMode === 'light' ? 'Dark' : 'Light'}</Button>
+                <HStack>
+
+                    <Button onClick={toggleColorMode}> Toggle {colorMode === 'light' ? 'Dark' : 'Light'}</Button>
+                    <Button
+                        onClick={sessionData ? () => signOut() : () => signIn()}
+                    >
+                        {sessionData ? 'Sign out' : 'Sign in'}
+                    </Button>
+                </HStack>
             </Flex>
         </chakra.header>
 
