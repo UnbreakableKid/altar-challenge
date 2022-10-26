2d4f0f3b5a69571234341c4478258998389fc86c
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2022-10-26 11:59:09 +0100

    oops let some cl

--- src/server/trpc/router/code.ts
+++ src/server/trpc/router/code.ts
@@ -44,13 +44,11 @@ export const codeRouter = router({
       //exception: if the count is larger than 9, divide the count by the lowest integer possible in order to get a value lower or equal to 9
       if (firstCharCount > 9) {
         const lowest = Math.ceil(firstCharCount / 9);
-        console.log(lowest);
         firstCharCount = Math.floor(firstCharCount / lowest);
       }
       //22
       if (secondCharCount > 9) {
         const lowest = Math.ceil(secondCharCount / 9);
-        console.log(lowest);
         secondCharCount = Math.floor(secondCharCount / lowest);
       }
       return {
